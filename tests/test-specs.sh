688d5471a798539424a1555554a1c9369450ab12
Iñaki Úcar <iucar@fedoraproject.org>
2026-01-12 23:40:55 +0100

    fix redirections

--- tests/test-specs.sh
+++ tests/test-specs.sh
@@ -5,9 +5,9 @@ fi
 
 cd specs
 for spec in *.spec; do
-    spectool -g $spec > /dev/null
+    spectool -g $spec > /dev/null 2>&1
     mock -q --rebuild --spec $spec --sources *.tar.gz \
-        -r fedora-rawhide-x86_64 --resultdir tmp > /dev/null
+        -r fedora-rawhide-x86_64 --resultdir tmp
     rm -f *.tar.gz tmp/*debug*.rpm tmp/*.src.rpm
     for rpm in tmp/*.rpm; do
         echo "RPM $rpm generated:"
