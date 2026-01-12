881297f5f75e0f3d94c3fcec41a368ac5a053215
Iñaki Úcar <iucar@fedoraproject.org>
2026-01-12 16:36:29 +0100

    add test for Provides

--- tests/test-R-deps.sh
+++ tests/test-R-deps.sh
@@ -4,3 +4,4 @@ echo anRpackage/DESCRIPTION | R-deps.R LinkingTo
 echo anRpackage/DESCRIPTION | R-deps.R Depends
 echo anRpackage/DESCRIPTION | R-deps.R Imports
 echo anRpackage/DESCRIPTION | R-deps.R Suggests
+echo anRpackage/DESCRIPTION | R-deps.R Provides
