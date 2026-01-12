25da7f774c5bcd805ec7e3b6080b1a84035b75f8
Iñaki Úcar <iucar@fedoraproject.org>
2026-01-12 16:43:42 +0100

    simplify test for R-deps

--- tests/test-R-deps.sh
+++ tests/test-R-deps.sh
@@ -1,16 +1,8 @@
 export PATH=../:$PATH
 
-echo "# LinkingTo"
-echo anRpackage/DESCRIPTION | R-deps.R LinkingTo
-echo "# Depends"
-echo anRpackage/DESCRIPTION | R-deps.R Depends
-echo "# Imports"
-echo anRpackage/DESCRIPTION | R-deps.R Imports
-echo "# LinkingTo Depends Imports"
-echo anRpackage/DESCRIPTION | R-deps.R LinkingTo Depends Imports
-echo "# Depends Imports"
-echo anRpackage/DESCRIPTION | R-deps.R Depends Imports
-echo "# Suggests"
-echo anRpackage/DESCRIPTION | R-deps.R Suggests
-echo "# Provides"
-echo anRpackage/DESCRIPTION | R-deps.R Provides
+for case in "LinkingTo" "Depends" "Imports" \
+            "LinkingTo Depends Imports" "Depends Imports" \
+            "Suggests" "Provides"; do
+    echo "# $case"
+    echo anRpackage/DESCRIPTION | R-deps.R $case
+done
