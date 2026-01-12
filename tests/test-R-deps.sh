6aaaf85e04efd5d543362608491e39bd2fe3f7da
Iñaki Úcar <iucar@fedoraproject.org>
2026-01-12 16:41:15 +0100

    test all combinations

--- tests/test-R-deps.sh
+++ tests/test-R-deps.sh
@@ -1,7 +1,16 @@
 export PATH=../:$PATH
 
+echo "# LinkingTo"
 echo anRpackage/DESCRIPTION | R-deps.R LinkingTo
+echo "# Depends"
 echo anRpackage/DESCRIPTION | R-deps.R Depends
+echo "# Imports"
 echo anRpackage/DESCRIPTION | R-deps.R Imports
+echo "# LinkingTo Depends Imports"
+echo anRpackage/DESCRIPTION | R-deps.R LinkingTo Depends Imports
+echo "# Depends Imports"
+echo anRpackage/DESCRIPTION | R-deps.R Depends Imports
+echo "# Suggests"
 echo anRpackage/DESCRIPTION | R-deps.R Suggests
+echo "# Provides"
 echo anRpackage/DESCRIPTION | R-deps.R Provides
