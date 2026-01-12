09306247765f0041ef4e862ed2d2733749f19612
Iñaki Úcar <iucar@fedoraproject.org>
2026-01-12 16:33:19 +0100

    add tests for R-deps

---
+++ tests/test-R-deps.sh
@@ -0,0 +1,6 @@
+export PATH=../:$PATH
+
+echo anRpackage/DESCRIPTION | R-deps.R LinkingTo
+echo anRpackage/DESCRIPTION | R-deps.R Depends
+echo anRpackage/DESCRIPTION | R-deps.R Imports
+echo anRpackage/DESCRIPTION | R-deps.R Suggests
